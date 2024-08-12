12a25997ae5683668ae52c4abbd1e984b438cf10
GIULIO ARECCO S334165 <s334165@studenti.polito.it>
2024-08-12 20:22:48 +0200

    Update Veloce di lavoro

--- vm/pt.c
+++ vm/pt.c
@@ -153,6 +153,7 @@ vm_fault(int faulttype, vaddr_t faultaddress)
 		/* Text segment pages must be readonly, so this can happen */
 		DEBUG(DB_VM, "VM_FAULT_READONLY\n");
 		// TODO: CLEAN PROCESS TERMINATION
+		// How do we terminate all proc threads?
 		break;
 	    case VM_FAULT_READ:
 		DEBUG(DB_VM, "VM_FAULT_READ\n");
