925d9e1af4161e94c6c052ecdff190368d819f27
GIULIO ARECCO S334165 <s334165@studenti.polito.it>
2024-08-12 17:10:52 +0200

    Update renamed tlb_loadpage in tlb_loadentry

--- include/vm_tlb.h
+++ include/vm_tlb.h
@@ -3,7 +3,7 @@
 
 #include <opt-paging.h>
 
-void tlb_loadpage(vaddr_t vaddr, paddr_t paddr, bool readonly);
+void tlb_loadentry(vaddr_t vaddr, paddr_t paddr, bool readonly);
 // uint32_t tlb_findfree(vaddr_t vaddr); <- Now static in vm_tlb.c
 // uint32_t tlb_get_rr_victim(void);     <- Now static in vm_tlb.c
 
