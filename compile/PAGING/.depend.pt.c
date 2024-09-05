341475107cd72ac228fc20d5dd580884be788cc4
LienoPC <albertocag9@gmail.com>
2024-09-05 09:58:00 +0200

    Reverting commit

--- compile/PAGING/.depend.pt.c
+++ compile/PAGING/.depend.pt.c
@@ -8,8 +8,9 @@ pt.o: ../../vm/pt.c ../../include/types.h ../../include/kern/types.h \
  ../../include/current.h includelinks/machine/current.h \
  ../../include/thread.h ../../include/array.h \
  includelinks/machine/thread.h ../../include/setjmp.h \
- includelinks/kern/machine/setjmp.h includelinks/mips/tlb.h \
- ../../include/elf.h includelinks/machine/elf.h ../../include/vnode.h \
+ includelinks/kern/machine/setjmp.h ../../include/syscall.h \
+ opt-syscalls.h opt-fork.h includelinks/mips/tlb.h ../../include/elf.h \
+ includelinks/machine/elf.h ../../include/vnode.h \
  ../../include/segments.h opt-paging.h ../../include/pt_fifo.h \
  ../../include/addrspace.h ../../include/vm.h ../../include/vm_tlb.h \
  ../../include/vfs.h ../../include/swapfile.h ../../include/coremap.h \
